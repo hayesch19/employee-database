90ea2e8850e931cf38f7a94964f6517f58d77f9d
hayesch19 <chrishayespro@gmail.com>
2019-09-08 19:45:05 -0400

    V4 Employee Page Info Displayed

--- src/pages/EmployeeProfile.jsx
+++ src/pages/EmployeeProfile.jsx
@@ -2,24 +2,37 @@ import React, { useEffect, useState } from 'react'
 import Axios from 'axios'
 
 const EmployeeProfile = props => {
-  const [profInfo, setProfInfo] = useState({ profile: [] })
   const [employeeId, setEmployeeId] = useState(props.match.params.employeeId)
+  const [profile, setProfile] = useState({})
 
   const fetchProfData = async () => {
     const resp = await Axios.get(
       `https://sdg-staff-directory-app.herokuapp.com/api/Unique%20Orns/employees/${employeeId}`
     )
-    setProfInfo(resp.data)
+    setProfile(resp.data)
     console.log({ data: resp.data })
   }
   useEffect(() => {
     fetchProfData()
-  }, [])
+  }, [employeeId])
+
+  useEffect(() => {
+    console.log(props)
+    console.log(props.match.params.employeeId)
+    setEmployeeId(props.match.params.employeeId)
+  }, [props])
 
   return (
-    <div>
-      <h3>Employee Profile</h3>
-    </div>
+    <section>
+      <div className="profile-info">
+        <h3>
+          {profile.firstName} {profile.lastName}
+        </h3>
+        <p>{profile.jobTitle}</p>
+        <p>{profile.email}</p>
+        <p>{profile.phoneNumber}</p>
+      </div>
+    </section>
   )
 }
 
